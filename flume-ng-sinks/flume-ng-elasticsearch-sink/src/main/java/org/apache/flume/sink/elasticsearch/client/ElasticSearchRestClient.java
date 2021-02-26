448c0f8d1592d233ee023c64ebb77369c38bbfcf
kim slbing <slbing@maplecloudy.com>
2021-02-26 11:28:46 +0800

    modify elasticsearch sink

--- flume-ng-sinks/flume-ng-elasticsearch-sink/src/main/java/org/apache/flume/sink/elasticsearch/client/ElasticSearchRestClient.java
+++ flume-ng-sinks/flume-ng-elasticsearch-sink/src/main/java/org/apache/flume/sink/elasticsearch/client/ElasticSearchRestClient.java
@@ -21,21 +21,28 @@ package org.apache.flume.sink.elasticsearch.client;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.sink.elasticsearch.ElasticSearchEventSerializer;
 import org.apache.flume.sink.elasticsearch.IndexNameBuilder;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.util.EntityUtils;
 import org.codehaus.jettison.json.JSONObject;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,104 +55,89 @@ import com.google.gson.Gson;
  * config params required should be taken through this.
  */
 public class ElasticSearchRestClient implements ElasticSearchClient {
-
-	private static final String INDEX_OPERATION_NAME = "index";
-	private static final String INDEX_PARAM = "_index";
-	private static final String TYPE_PARAM = "_type";
-	private static final String BULK_ENDPOINT = "_bulk";
-
-	private static final Logger logger = LoggerFactory.getLogger(ElasticSearchRestClient.class);
-
-	private final ElasticSearchEventSerializer serializer;
-	private final RoundRobinList<String> serversList;
-
-	private StringBuilder bulkBuilder;
-	private HttpClient httpClient;
-
-	public ElasticSearchRestClient(String[] hostNames, ElasticSearchEventSerializer serializer) {
-
-		for (int i = 0; i < hostNames.length; ++i) {
-			if (!hostNames[i].contains("http://") && !hostNames[i].contains("https://")) {
-				hostNames[i] = "http://" + hostNames[i];
-			}
-		}
-		this.serializer = serializer;
-
-		serversList = new RoundRobinList<String>(Arrays.asList(hostNames));
-		httpClient = new DefaultHttpClient();
-		bulkBuilder = new StringBuilder();
-	}
-
-	@VisibleForTesting
-	public ElasticSearchRestClient(String[] hostNames, ElasticSearchEventSerializer serializer, HttpClient client) {
-		this(hostNames, serializer);
-		httpClient = client;
-	}
-
-	@Override
-	public void configure(Context context) {
-	}
-
-	@Override
-	public void close() {
-	}
-
-	@Override
-	public void addEvent(Event event, IndexNameBuilder indexNameBuilder, String indexType, long ttlMs)
-			throws Exception {
-		try {
-			JSONObject content = serializer.getContent(event);
-			if(content == null)
-				return;
-
-			Map<String, Map<String, String>> parameters = new HashMap<String, Map<String, String>>();
-			Map<String, String> indexParameters = new HashMap<String, String>();
-			indexParameters.put(INDEX_PARAM, indexNameBuilder.getIndexName(event));
-//			indexParameters.put(TYPE_PARAM, indexType);
-			parameters.put(INDEX_OPERATION_NAME, indexParameters);
-
-			Gson gson = new Gson();
-			synchronized (bulkBuilder) {
-				bulkBuilder.append(gson.toJson(parameters));
-				bulkBuilder.append("\n");
-				bulkBuilder.append(content.toString());
-				bulkBuilder.append("\n");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("skip this event:"+e.getMessage());
-		}
-	}
-
-	@Override
-	public void execute() throws Exception {
-		int statusCode = 0, triesCount = 0;
-		HttpResponse response = null;
-		String entity;
-		synchronized (bulkBuilder) {
-			entity = bulkBuilder.toString();
-			bulkBuilder = new StringBuilder();
-		}
-
-		while (statusCode != HttpStatus.SC_OK && triesCount < serversList.size()) {
-			triesCount++;
-			String host = serversList.get();
-			String url = host + "/" + BULK_ENDPOINT;
-			HttpPost httpRequest = new HttpPost(url);
-//			httpRequest.setHeader("Content-tpye", "	application/json;charset:UTF-8");
-//			httpRequest.setHeader("", "	application/json");
-			
-			httpRequest.setEntity(new StringEntity(new String(entity.getBytes(),"utf-8"),"utf-8"));
-//			System.out.println("---------"+entity);
-			response = httpClient.execute(httpRequest);
-			
-			statusCode = response.getStatusLine().getStatusCode();
-			
-			logger.info("Status code from elasticsearch: " + statusCode);
-			if (response.getEntity() != null) {
-				EntityUtils.consume(response.getEntity());
-				logger.debug("Status message from elasticsearch: " + response.toString());
-			}
-		}
-	}
+  
+  
+  
+  private static final Logger logger = LoggerFactory
+      .getLogger(ElasticSearchRestClient.class);
+  
+  private final ElasticSearchEventSerializer serializer;
+  
+  BulkRequest bulkRequest = new BulkRequest();
+  
+  private RestHighLevelClient client = null;
+  
+  public ElasticSearchRestClient(String[] hostNames,
+      ElasticSearchEventSerializer serializer) {
+    
+    HttpHost[] httpHosts = new HttpHost[hostNames.length];
+    for (int i = 0; i < hostNames.length; i++) {
+      httpHosts[i] = HttpHost.create(hostNames[i]);
+    }
+    RestClientBuilder builder = RestClient.builder(httpHosts);
+    client = new RestHighLevelClient(builder);
+    this.serializer = serializer;
+    
+  }
+  
+  @VisibleForTesting
+  public ElasticSearchRestClient(String[] hostNames,
+      ElasticSearchEventSerializer serializer, RestHighLevelClient client) {
+    this(hostNames, serializer);
+    this.client = client;
+  }
+  
+  @Override
+  public void configure(Context context) {}
+  
+  @Override
+  public void close() {}
+  
+  @Override
+  public void addEvent(Event event, IndexNameBuilder indexNameBuilder,
+      String indexType, long ttlMs) throws Exception {
+    try {
+      JSONObject content = serializer.getContent(event);
+      if (content == null) return;
+      
+      IndexRequest indexRequest = new IndexRequest(
+          indexNameBuilder.getIndexName(event), indexType).source(content);
+      
+      synchronized (bulkRequest) {
+        bulkRequest.add(indexRequest);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      logger.error("skip this event:" + e.getMessage());
+    }
+  }
+  
+  public static class BulkActionListenerc
+      implements ActionListener<BulkResponse> {
+    
+    @Override
+    public void onResponse(BulkResponse resp) {
+      for (BulkItemResponse bir : resp.getItems()) {
+        if (bir.isFailed()) {
+          logger.info("index failed with:" + bir.getFailureMessage());
+        }
+      }
+    }
+    
+    @Override
+    public void onFailure(Exception e) {
+      logger.info(ExceptionUtils.getFullStackTrace(e));
+    }
+    
+  }
+  
+  @Override
+  public void execute() throws Exception {
+    
+    synchronized (bulkRequest) {
+      client.bulkAsync(bulkRequest, RequestOptions.DEFAULT,
+          new BulkActionListenerc());
+      bulkRequest = new BulkRequest();
+    }
+  }
 }
